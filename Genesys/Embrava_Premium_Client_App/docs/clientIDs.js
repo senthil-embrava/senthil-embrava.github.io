7bc917ecb9545585e79fcd23782d86eaa7b20996
EAGLESWIN-LP-01\Senthil <senthil@embrava.com>
2018-09-23 17:49:41 +0530

    Adding the app repo

---
+++ Genesys/Embrava_Premium_Client_App/docs/clientIDs.js
@@ -0,0 +1,6 @@
+export default {
+    // 'mypurecloud.com': 'e88db007-526a-4f21-af25-81118183c37e', //Development Client ID
+    'mypurecloud.com': '76863e4f-44c7-4fd2-9fd6-1ad4945a89fc',
+    'mypurecloud.ie': '8233c135-c488-427c-b301-158b164f2e4d',
+    'mypurecloud.com.au': '173ed139-b09c-4189-b206-97ef3dce4d16'
+}
